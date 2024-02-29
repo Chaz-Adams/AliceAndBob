82f970e399f3b79353c9311939c41c4f8e59fc85
Chaz-Adams <caadams1922@gmail.com>
2024-02-29 14:52:34 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
@@ -14,12 +16,18 @@ public class Main {
 
     public void compute() {
         // Get input string of name from user
-        
-        // Create conditional to check name against Alice and Bob
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
 
-        // Print greeting to screen if Alice or Bob are true
-        
-        // Print "You are neither Alice nor Bob." otherwise
+        // Create conditional to check name against Alice and Bob
+        if(name.equals("Alice")||name.equals("Bob")){
+            // Print greeting to screen if Alice or Bob are true
+            System.out.println("Hello "+name+".");
+        }else {
+            System.out.println("You are neither Alice nor Bob.");
+        }// Print "You are neither Alice nor Bob." otherwise
+
 
     }
 }
